6e211d3574a6bf0f627ef6dac43854e20b02d05a
Gabriel Dinard <gabrieldinard@gmail.com>
2020-05-27 08:58:19 -0300

    Contém os dados necessários para cadastrar uma pessoa

---
+++ Aniversario.Model/Pessoa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace Aniversario.Model
+
+{
+    public class Pessoa
+    {
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public DateTime DataDeAniversario { get; set; }
+
+        public Pessoa(string nome, string sobrenome, DateTime dataDeAniversario)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+            DataDeAniversario = dataDeAniversario;
+        }
+    }
+}

cc56da4938b49b8dbacf912d6d920871c62a016f
Gabriel Dinard <gabrieldinard@gmail.com>
2020-05-27 08:58:49 -0300

    Contém o banco de dados abstrato e o em memória

---
+++ Aniversario.Dados/BancoDeDados.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Aniversario.Model;
+
+namespace Aniversario.Dados
+{
+    public abstract class BancoDeDados
+    {
+        public abstract void Salvar(Pessoa pessoa);
+        public abstract void Excluir(Pessoa pessoa);
+        public abstract IEnumerable<Pessoa> BuscarTodasAsPessoas();
+        public abstract IEnumerable<Pessoa> BuscarTodasAsPessoas(string nome);
+        public abstract IEnumerable<Pessoa> BuscarTodasAsPessoas(DateTime dataDeAniversario);
+        public abstract IEnumerable<Pessoa> BuscarPessoaPelo(string sobrenome);
+    }
+}
---
+++ Aniversario.Dados/BancoDeDadosEmMemoria.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using Aniversario.Model;
+
+namespace Aniversario.Dados
+{
+    public class BancoDeDadosEmMemoria : BancoDeDados
+    {
+        private static List<Pessoa> pessoasCadastradas = new List<Pessoa>();
+
+        public override void Salvar(Pessoa pessoa)
+        {
+            bool pessoaJaExisteNaListaDeCadastrados = false;
+
+            foreach (var pess in pessoasCadastradas)
+            {
+                if (pess == pessoa)
+                {
+                    pessoaJaExisteNaListaDeCadastrados = true;
+                }
+            }
+
+            if (pessoaJaExisteNaListaDeCadastrados == false)
+            {
+                pessoasCadastradas.Add(pessoa);
+            }
+        }
+
+        public override IEnumerable<Pessoa> BuscarTodasAsPessoas()
+        {
+            return pessoasCadastradas;
+        }
+
+        public override IEnumerable<Pessoa> BuscarTodasAsPessoas(string nome)
+        {
+            //dica: nas consultas retornas ienumerable
+            return pessoasCadastradas
+                   .Where(pessoa => pessoa.Nome.Contains(nome, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public override IEnumerable<Pessoa> BuscarTodasAsPessoas(DateTime dataDeAniversario)
+        {
+            return pessoasCadastradas
+                   .Where(pessoa => pessoa.DataDeAniversario.Date == dataDeAniversario);
+        }
+
+        public override IEnumerable<Pessoa> BuscarPessoaPelo(string sobrenome)
+        {
+            return pessoasCadastradas
+                    .Where(pessoa => pessoa.Sobrenome.Contains(sobrenome, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        public override void Excluir(Pessoa pessoa)
+        {
+            pessoasCadastradas.Remove(pessoa);
+        }       
+    }
+}
